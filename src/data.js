d57d85956e6c606558e23752ad0aecf6b4b1d0b8
Толстолыткин Александр <a-tolstolytkin@mail.ru>
2024-12-19 19:11:05 +0300

    Реализация функционала

--- src/data.js
+++ src/data.js
@@ -1,6 +1,73 @@
+import soundWinter from "./assets/sounds/winter.mp3";
+import soundSummer from "./assets/sounds/summer.mp3";
+import soundRain from "./assets/sounds/rain.mp3";
+import iconWinter from "./assets/icons/cloud-snow.svg";
+import iconSummer from "./assets/icons/sun.svg";
+import iconRain from "./assets/icons/cloud-rain.svg";
+import bgWinter from "./assets/winter-bg.jpg";
+import bgSummer from "./assets/summer-bg.jpg";
+import bgRain from "./assets/rainy-bg.jpg";
+
 export default [
-  { id: 1, title: "Item 1" },
-  { id: 2, title: "Item 2" },
-  { id: 3, title: "Item 3" },
-  { id: 4, title: "Item 4" },
+  {
+    id: 1,
+    weather: "winter",
+    sound: soundWinter,
+    icons: iconWinter,
+    background: bgWinter,
+  },
+  {
+    id: 2,
+    weather: "summer",
+    sound: soundSummer,
+    icons: iconSummer,
+    background: bgSummer,
+  },
+  {
+    id: 3,
+    weather: "rain",
+    sound: soundRain,
+    icons: iconRain,
+    background: bgRain,
+  },
 ];
+
+HTMLAudioElement.prototype.stop = function () {
+  this.pause();
+  this.currentTime = 0.0;
+};
+const divCheck = document.getElementById("app");
+
+let prevAudio = null;
+let value = "";
+let flagSound = false;
+
+divCheck.addEventListener("click", ({ target }) => {
+  const isAudio = target.querySelector("audio");
+  if (target.value) {
+    return;
+  }
+  if (value !== target.classList.value && value !== "") {
+    flagSound = false;
+    prevAudio.stop();
+    value = target.classList.value;
+    prevAudio = isAudio;
+    isAudio.play();
+    return;
+  }
+  if (flagSound) {
+    prevAudio = isAudio;
+    flagSound = false;
+    isAudio.play();
+    return;
+  }
+  if (value === "" || value !== target.classList.value) {
+    prevAudio = isAudio;
+    value = target.classList.value;
+    isAudio.play();
+  } else if (value === target.classList.value) {
+    prevAudio = isAudio;
+    flagSound = true;
+    isAudio.pause();
+  }
+});
--- src/index.js
+++ src/index.js
@@ -4,12 +4,35 @@ import "./index.scss";
 const root = document.querySelector("#app");
 
 function renderItem(item, index) {
-  const li = document.createElement("li");
-  li.textContent = item.title;
-  if (index % 2 === 0) {
-    li.style.color = "red";
-  }
-  root.append(li);
+  const divWindow = document.createElement("div");
+  divWindow.classList.add("diwWindow");
+
+  const myAudio = new Audio(item.sound);
+
+  const button = document.createElement("button");
+  button.innerHTML = `<img src=${item.icons} width="40px" pointer-events: none/>`;
+  button.classList.add(`btn${item.weather}`);
+
+  const divCheck = document.querySelector("body");
+
+  divWindow.addEventListener("click", () => {
+    divCheck.style.backgroundImage = `url(${item.background})`;
+  });
+
+  const range = document.getElementById("range");
+  range.addEventListener("input", () => {
+    myAudio.volume = Number(range.value) / 100;
+  });
+
+  // let volumes = Number(target.value);
+  // console.log(volumes);
+
+  // console.log(target.value);
+  // isAudio.volume = volumes;
+
+  button.appendChild(myAudio);
+  divWindow.appendChild(button);
+  root.append(divWindow);
 }
 
 data.forEach(renderItem);
